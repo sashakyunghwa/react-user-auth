bccd05c6b6d03458bef0b3fb6043005709b61954
Queen Bae <sashakyunghwa@gmail.com>
2018-04-13 18:18:57 -0700

    Create account completed

---
+++ src/reducers/form_reducer.js
@@ -0,0 +1,19 @@
+import types from '../actions/types';
+
+const DEFAULT_STATE = {
+    values: {}, 
+    errors: []
+};
+
+export default (state = DEFAULT_STATE, action) => {
+    console.log('Form State:', state);
+    switch(action.type){
+        case types.INPUT_CHANGE:
+            return {...state, values: {...state.values, [action.name]: action.value}};
+        case types.FORM_ERROR:
+            return {...state, errors: action.error};
+        default: 
+            return state;
+    }
+}
+
